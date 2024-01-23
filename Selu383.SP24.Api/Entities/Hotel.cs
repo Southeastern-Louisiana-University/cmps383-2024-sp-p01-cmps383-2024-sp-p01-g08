c3af5fe33b1655e1ef70bf95d91125d8d034d025
Jai Brown <jai.brown@selu.edu>
2024-01-23 00:10:16 -0600

    added hotel entity, started data context setup

---
+++ Selu383.SP24.Api/Entities/Hotel.cs
@@ -0,0 +1,28 @@
+﻿namespace Selu383.SP24.Api.Entities
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class HotelGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class HotelCreateDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class HotelUpdateDto
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
--- Selu383.SP24.Api/Program.cs
+++ Selu383.SP24.Api/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
 
 // Add services to the container.
 
